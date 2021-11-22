67a9ba284d1f24b13e5a28bada7aae90af18ce25
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-22 19:54:06 +0300

    Getting values from a url

--- news/views.py
+++ news/views.py
@@ -29,3 +29,17 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+
+def past_days_news(request,past_date):
+        # Converts data from the string Url
+    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
