ac7a4af0714329fe052167e9fb096ec154c74ea4
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-23 00:25:14 +0300

    Child templates that extend base url

--- news/views.py
+++ news/views.py
@@ -1,34 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse, Http404
 import datetime as dt
 
 
-def past_days_news(request,past_date):
+# View Function to present news from past days
+def past_days_news(request, past_date):
 
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
 
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
+    
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
     
 def news_of_day(request):
     date = dt.date.today()
-     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-news/today-news.html', {"date": date,})
 
 def convert_dates(dates):
 
