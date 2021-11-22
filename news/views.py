2b6bbce3a44b17f4a7b64be5132c6cec34d878a8
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-22 19:41:08 +0300

    Getting the day of the week

--- news/views.py
+++ news/views.py
@@ -8,13 +8,24 @@ def welcome(request):
 
 def news_of_day(request):
     date = dt.date.today()
+     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
     return HttpResponse(html)
 
+def convert_dates(dates):
 
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
