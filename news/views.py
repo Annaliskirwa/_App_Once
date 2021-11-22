ebe7261c0b08d82e005d1258028722e54be87f0c
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-22 19:56:44 +0300

    error handling to make sure correct date is passed

--- news/views.py
+++ news/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse, Http404
 import datetime as dt
+
+
+def past_days_news(request,past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
 
 # Create your views here.
 def welcome(request):
