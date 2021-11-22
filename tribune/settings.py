ff8ca091d58c7d9fc40b818bf898eaa630942da8
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-23 01:16:29 +0300

    Change project database settings to use Postgres

--- tribune/settings.py
+++ tribune/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tribune',
+        'USER': 'annalis',
+    'PASSWORD':'Ann123',
     }
 }
 
