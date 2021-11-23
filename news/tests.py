ae7e0c421b5c3025a8d24ab64be6391337c5be94
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-23 23:35:25 +0300

    Creating an article test class

--- news/tests.py
+++ news/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Editor,tags
+from .models import Editor,tags,Article
 
 # Create your tests here.
 class EditorTestClass(TestCase):
@@ -23,3 +23,25 @@ class EditorTestClass(TestCase):
     #     self.james.delete_editor()
     #     editors = Editor.objects.all()
     #     self.assertTrue(editors)
+
+
+class ArticleTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
