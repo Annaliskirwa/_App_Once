6d74eebde6c472cc98e84124b1e97ee3c7eac4e1
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-23 07:11:29 +0300

    Testing the save method

--- news/tests.py
+++ news/tests.py
@@ -11,3 +11,9 @@ class EditorTestClass(TestCase):
 # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.james,Editor))
+
+# Testing Save Method
+    def test_save_method(self):
+        self.james.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
