46900e9bcc48b1f214e528f283d1653daf5d0f48
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-23 06:44:59 +0300

    Setting up tests

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+
