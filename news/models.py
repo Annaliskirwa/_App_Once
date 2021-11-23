7aefa0b48c9fa763d944ed26ab69b0de9a75d57b
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-23 06:31:55 +0300

    Read models in shell

--- news/models.py
+++ news/models.py
@@ -4,3 +4,6 @@ class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
