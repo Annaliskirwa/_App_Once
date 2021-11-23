7131fbeab035fbec940220df22ab711cd3a6950b
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-23 23:07:28 +0300

    Adding models to admin page

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Editor,tags
 
-# Register your models here.
+admin.site.register(Editor)
+# admin.site.register(Article)
+admin.site.register(tags)
