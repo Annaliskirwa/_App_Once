46355e947be4eec90c3fbfa3978a1f217d919c83
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-24 07:28:16 +0300

    Single article

--- news/views.py
+++ news/views.py
@@ -71,3 +71,9 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html',{"message":message})
+def article(request,article_id):
+    try:
+        article = Article.objects.get(id = article_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-news/article.html", {"article":article})
