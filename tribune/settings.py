839f33ef425393ed76b92366fda789a7f24abfaa
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-25 07:25:47 +0300

    enable gzip functionality

--- tribune/settings.py
+++ tribune/settings.py
@@ -130,6 +130,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
