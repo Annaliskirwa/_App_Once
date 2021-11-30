618a213a18abea7609f0adb194bae58c1009eab5
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-30 17:52:10 +0300

    Welcome email function called in view

--- news/views.py
+++ news/views.py
@@ -4,6 +4,7 @@ import datetime as dt
 from .models import Article, NewsLetterRecipients
 from .forms import NewsLetterForm
 from django.core.exceptions import ObjectDoesNotExist
+from . emails import send_welcome_email
 
 
 # View Function to present news from past days
@@ -63,11 +64,13 @@ def news_today(request):
         if form.is_valid():
             name = form.cleaned_data['your_name']
             email = form.cleaned_data['email']
+
             recipient = NewsLetterRecipients(name = name,email =email)
             recipient.save()
+            send_welcome_email(name,email)
+
             HttpResponseRedirect('news_today')
-    else:
-        form = NewsLetterForm()
+            #.................
     return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
     
 def search_results(request):
