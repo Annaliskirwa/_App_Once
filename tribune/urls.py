079021c19faac4bebc4b66d63dd3d06cf4888f63
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-30 19:42:19 +0300

    logout functionality we need to implement a logout view

--- tribune/urls.py
+++ tribune/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views 
 
 urlpatterns = [
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'',include('news.urls'))
+    url(r'',include('news.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'})
 ]
 
