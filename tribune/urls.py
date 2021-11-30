b22062e6243d12007488ce54225c336949f68f9e
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-30 20:17:43 +0300

    Log out view

--- tribune/urls.py
+++ tribune/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'',include('news.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'})
+    url('logout/', views.LogoutView.as_view(), name='logout'),
 ]
 
