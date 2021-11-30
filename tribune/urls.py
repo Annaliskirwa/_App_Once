31a2d9281d61569214f2bd5f6c06b7cff72b6f97
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-30 18:03:52 +0300

    Configure url for registration

--- tribune/urls.py
+++ tribune/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'',include('news.urls'))
 ]
