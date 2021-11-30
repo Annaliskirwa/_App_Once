cfafd7fe1a428b253bb6ef5281516d0279077f46
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-30 14:31:16 +0300

    Creating a newsletter

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse, Http404
 import datetime as dt
 from .models import Article
+from .forms import NewsLetterForm
 
 
 # View Function to present news from past days
@@ -57,8 +58,14 @@ def past_days_news(request,past_date):
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
-
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
+    
 def search_results(request):
 
     if 'article' in request.GET and request.GET["article"]:
