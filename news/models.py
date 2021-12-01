3f437ef86a56c2c53933aa15824e5cfbe5abdc4b
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-01 08:30:02 +0300

    Changing from timyce to textfield to input a post

--- news/models.py
+++ news/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # class Editor(models.Model):
 #     first_name = models.CharField(max_length =30)
@@ -27,6 +28,7 @@ class tags(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
+#     post = HTMLField()
     editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
