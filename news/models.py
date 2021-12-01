aaeb6e09717eee423d5ad8bb7564713b8a82860a
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-01 07:22:37 +0300

    Applying migration after deleting editor model

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
 
 # class Editor(models.Model):
 #     first_name = models.CharField(max_length =30)
@@ -26,7 +27,7 @@ class tags(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
-    editor = models.ForeignKey(Editor,on_delete = models.CASCADE)
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
     article_image = models.ImageField(upload_to = 'articles/', null=True)
