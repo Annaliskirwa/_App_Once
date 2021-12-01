d71dfd1e09248ed9c852830e61eaa7236c35d51b
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-01 07:17:19 +0300

    Removing the editor model

--- news/models.py
+++ news/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 import datetime as dt
 
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length = 10,blank =True)
+# class Editor(models.Model):
+#     first_name = models.CharField(max_length =30)
+#     last_name = models.CharField(max_length =30)
+#     email = models.EmailField()
+#     phone_number = models.CharField(max_length = 10,blank =True)
 
-    def __str__(self):
-        return self.first_name
-    class Meta:
-        ordering = ['first_name']
-    def save_editor(self):
-        self.save()
+#     def __str__(self):
+#         return self.first_name
+#     class Meta:
+#         ordering = ['first_name']
+#     def save_editor(self):
+#         self.save()
 
-    # def delete_editor(self):
-    #     self.delete()
+#     # def delete_editor(self):
+#     #     self.delete()
 
 class tags(models.Model):
     name = models.CharField(max_length =30)
