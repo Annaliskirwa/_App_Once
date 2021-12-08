a2c80b58df74d60f7486f9bc9097585f18d9d078
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-08 14:46:41 +0300

    Create a new view function to display the name and email from ajax request

--- news/views.py
+++ news/views.py
@@ -65,6 +65,16 @@ def news_today(request):
     form = NewsLetterForm()
     return render(request, 'all-news/today-news.html', {"date": date, "news": news, "letterForm": form})
 
+def newsletter(request):
+    name = request.POST.get('your_name')
+    email = request.POST.get('email')
+
+    recipient = NewsLetterRecipients(name=name, email=email)
+    recipient.save()
+    send_welcome_email(name, email)
+    data = {'success': 'You have been successfully added to mailing list'}
+    return JsonResponse(data)
+
 def search_results(request):
 
     if 'article' in request.GET and request.GET["article"]:
