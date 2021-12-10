b5c3ccc339022452b87129a9951eaf6d319f7738
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-10 09:16:58 +0300

    Added rest framework authtoken as an app

--- news/views.py
+++ news/views.py
@@ -129,6 +129,6 @@ class MerchList(APIView):
         serializers = MerchSerializer(data=request.data)
         if serializers.is_valid():
             serializers.save()
-            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
