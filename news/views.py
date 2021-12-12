aa5e2d1878981036222d4254987edd28239fc302
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-12 17:30:34 +0300

    Adding permissions to API View class

--- news/views.py
+++ news/views.py
@@ -13,6 +13,7 @@ from rest_framework.views import APIView
 from .models import  MoringaMerch
 from .serializer import MerchSerializer
 from rest_framework import status
+from .permissions import IsAdminOrReadOnly
 
 
 
@@ -131,4 +132,6 @@ class MerchList(APIView):
             serializers.save()
         return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    permission_classes = (IsAdminOrReadOnly,)
 
